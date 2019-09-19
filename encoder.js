fa10c45d57cf1b4cd882fa3efd2971961ea8f755
Devon <miss.edwards.joseph@gmail.com>
2019-09-19 20:18:28 +0100

    week2: Added proxy var to cipher class

--- encoder.js
+++ encoder.js
@@ -1,6 +1,6 @@
-// Encoder applies the Ceasar Cipher to a message
+// Ceasar Cipher class defined
 
-class CeasarCipherEncoder {
+class CeasarCipher {
   constructor(message) {
     this.alphabet = [
       "A",
@@ -34,20 +34,20 @@ class CeasarCipherEncoder {
     let cipherLetterIndex;
     let alphabet = this.alphabet;
 
-    // Proxy is used to allow the use of negative array indexes in applyEncoding  
-    const negIndexProxy = new Proxy(alphabet, {
-        get(targetLetter, prop) {
-            if (!isNaN(prop)) {
-                prop = parseInt(prop, 10);
-                if (prop < 0) {
-                    prop += targetLetter.length;
-                }
-            }
-            return targetLetter[prop];
-        }
-    });
-
     for (letter of mssg) {
+
+        // Proxy is used to allow the use of negative array indexes in applyEncoding  
+        let negIndexProxy = new Proxy(alphabet, {
+            get(targetLetter, prop) {
+                if (!isNaN(prop)) {
+                    prop = parseInt(prop, 10);
+                    if (prop < 0) {
+                        prop += targetLetter.length;
+                    }
+                }
+                return targetLetter[prop];
+            }
+        });
 
         // TODO: Add exception handling for message characters that are not letters
         letter = letter.toUpperCase;
@@ -57,9 +57,11 @@ class CeasarCipherEncoder {
 
         // Replace the original message letter with the encoding letter
         console.log(negIndexProxy[cipherLetterIndex])
-        //mssg = mssg.replace(letter, alphabet[cipherLetterIndex]);
 
-        //console.log(mssg)
+        // TODO: Change to only print the fully encoded string - don't print mssg at each run of the loop
+        mssg = mssg.replace(letter, alphabet[cipherLetterIndex]);
+
+        console.log(mssg)
     }
 
     console.log(`...Completed message encoding.`);
@@ -69,7 +71,7 @@ class CeasarCipherEncoder {
 
 
 
-const myEncoder = new CeasarCipherEncoder();
+const myEncoder = new CeasarCipher();
 
 let encodedMessage = myEncoder.applyEncoding(`This is my top secret message.`);
 console.log(encodedMessage);
