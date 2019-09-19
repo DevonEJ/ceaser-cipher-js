ae6877a5a9722b6f636ab66ac56168d0dd917b62
Devon <miss.edwards.joseph@gmail.com>
2019-09-19 20:19:05 +0100

    week2: Adding code formatting

--- encoder.js
+++ encoder.js
@@ -35,47 +35,39 @@ class CeasarCipher {
     let alphabet = this.alphabet;
 
     for (letter of mssg) {
-
-        // Proxy is used to allow the use of negative array indexes in applyEncoding  
-        let negIndexProxy = new Proxy(alphabet, {
-            get(targetLetter, prop) {
-                if (!isNaN(prop)) {
-                    prop = parseInt(prop, 10);
-                    if (prop < 0) {
-                        prop += targetLetter.length;
-                    }
-                }
-                return targetLetter[prop];
+      // Proxy is used to allow the use of negative array indexes in applyEncoding
+      let negIndexProxy = new Proxy(alphabet, {
+        get(targetLetter, prop) {
+          if (!isNaN(prop)) {
+            prop = parseInt(prop, 10);
+            if (prop < 0) {
+              prop += targetLetter.length;
             }
-        });
+          }
+          return targetLetter[prop];
+        }
+      });
 
-        // TODO: Add exception handling for message characters that are not letters
-        letter = letter.toUpperCase;
+      // TODO: Add exception handling for message characters that are not letters
+      letter = letter.toUpperCase;
 
-        // Get Index of the letter 3 letters before the letter used in the message     
-        cipherLetterIndex = alphabet.indexOf(letter) - 3;
+      // Get Index of the letter 3 letters before the letter used in the message
+      cipherLetterIndex = alphabet.indexOf(letter) - 3;
 
-        // Replace the original message letter with the encoding letter
-        console.log(negIndexProxy[cipherLetterIndex])
+      // Replace the original message letter with the encoding letter
+      console.log(negIndexProxy[cipherLetterIndex]);
 
-        // TODO: Change to only print the fully encoded string - don't print mssg at each run of the loop
-        mssg = mssg.replace(letter, alphabet[cipherLetterIndex]);
+      // TODO: Change to only print the fully encoded string - don't print mssg at each run of the loop
+      mssg = mssg.replace(letter, alphabet[cipherLetterIndex]);
 
-        console.log(mssg)
+      console.log(mssg);
     }
 
     console.log(`...Completed message encoding.`);
   }
 }
 
-
-
-
 const myEncoder = new CeasarCipher();
 
 let encodedMessage = myEncoder.applyEncoding(`This is my top secret message.`);
 console.log(encodedMessage);
-
-
-
-
